492754664d7391c4854e7996f32a3ecbe1676120
Leon Oostrum <leonoostrum@gmail.com>
2018-03-08 12:34:02 +0000

    Added plotter, including timeseries

--- plotter.py
+++ plotter.py
@@ -4,6 +4,7 @@
 
 import sys
 
+import numpy as np
 import h5py
 import matplotlib
 matplotlib.use('pdf')
@@ -16,18 +17,22 @@ if __name__ == '__main__':
 
     # read dataset 
     with h5py.File(fname, 'r') as f:
-        data_frb_candidate = f['data_frb_candidate']
-        probability = f['probability']
-        params = f['params']  # snr, DM, boxcard width, arrival time
+        data_frb_candidate = f['data_frb_candidate'][:]
+        probability = f['probability'][:]
+        params = f['params'][:]  # snr, DM, boxcard width, arrival time
 
-    for i, cand in data_frb_candidate:
+    for i, cand in enumerate(data_frb_candidate):
         data_freq_time = cand[:, :, 0]
         prob = probability[i]
         snr, dm, boxcar_width, t0 = params[i]
 
-        fig, ax = plt.subplots()
+        fig, (ax1, ax2) = plt.subplots(nrows=2, sharex=True, gridspec_kw=dict(height_ratios=[1, 2]))
+
+        # timeseries
+        ax1.plot(np.average(data_freq_time, axis=0), c='k')
+        # waterfall plot
         # scaling: std = 1, median=0
-        ax.imshow(data_freq_time, cmap='viridis', vmin=-3, vmax=3, interpolation='nearest', aspect='equal')
-        fig.suptitle("p: {:.1f} }SNR: {:.0f} DM: {:.2f}, T0: {:.2f}".format(prob, snr, dm, t0))
-        plt.savefig("plots/cand_{:2d}_snr{:.0f}_dm{:.0f}.pdf".format(i, snr, dm))
+        ax2.imshow(data_freq_time, cmap='viridis', vmin=-3, vmax=3, interpolation='nearest', aspect='equal')
+        fig.suptitle("p: {:.1f} SNR: {:.0f} DM: {:.2f}, T0: {:.2f}".format(prob, snr, dm, t0))
+        plt.savefig("plots/cand_{:04d}_snr{:.0f}_dm{:.0f}.pdf".format(i, snr, dm))
         plt.close(fig)
--- process_triggers.sh
+++ process_triggers.sh
@@ -22,14 +22,14 @@ prefix=$3
 snrmin=$4
 
 # create master trigger files
-cat ${prefix}*trigger > ${prefix}.trigger
+cat ${prefix}_step*trigger > ${prefix}.trigger
 
 mkdir -p $outputdir/plots
 cd $outputdir
 source $HOME/venv/bin/activate
 # process the triggers without making plots
 #python $triggerscript --sig_thresh $snrmin --ndm $ndm --save_data $fmt --mk_plot --ntrig $ntrig --nfreq_plot $nfreq_plot --ntime_plot $ntime_plot --cmap $cmap $filfile $prefix.trigger
-python $triggerscript --sig_thresh $snrmin --ndm $ndm --save_data $fmt --mk_plot=False --ntrig $ntrig --nfreq_plot $nfreq_plot --ntime_plot $ntime_plot --cmap $cmap $filfile ${prefix}.trigger
+python $triggerscript --sig_thresh $snrmin --ndm $ndm --save_data $fmt --ntrig $ntrig --nfreq_plot $nfreq_plot --ntime_plot $ntime_plot --cmap $cmap $filfile ${prefix}.trigger
 # concatenate hdf5 files
 python $preproc --fnout combined.hdf5 --nfreq_f $nfreq_plot --ntime_f $ntime_plot $(pwd)
 deactivate
@@ -41,7 +41,7 @@ python $classifier combined.hdf5
 deactivate
 # make plots
 source $HOME/venv/bin/activate
-python $plotter combinedfreq_time_candidates.hdf5
+python $plotter combinefreq_time_candidates.hdf5
 deactivate
 # merge 
 gs -dBATCH -dNOPAUSE -q -sDEVICE=pdfwrite -sOutputFile=candidates.pdf plots/*pdf
