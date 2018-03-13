aa10edb8e89eb5379bd54862d581425e4a524f51
Leon Oostrum <leonoostrum@gmail.com>
2018-03-13 11:06:46 +0000

    Added script to get RA/DEC of known PSR

---
+++ control/psr_ra_dec.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+psr=$1
+
+if [ -z $psr ]; then
+    echo "Specify pulsar name as first argument"
+    exit
+fi
+
+RA=$(psrcat -e $psr | grep RAJ | awk '{echo $2}')
+DEC=$(psrcat -e $psr | grep DECJ | awk '{echo $2}')
+
+if [ -z $RA ]; then
+    echo "Warning: coordinates not found for PSR $psr"
+    RA=00:00:00
+    DEC=00:00:00
+fi
+
+echo --source $psr --ra $RA --dec $DEC
