a55529db5a260b076670f4223ee86656aa467018
Leon Oostrum <leonoostrum@gmail.com>
2018-03-14 20:52:52 +0000

    back to 4 ambers, full subband tuned conf

--- start_survey_master.py
+++ start_survey_master.py
@@ -94,9 +94,9 @@ def start_survey(args):
     pars['chan_width'] = float(pars['bw']) / pars['nchan']
     pars['min_freq'] = pars['freq'] - pars['bw'] / 2 + pars['chan_width'] / 2
     if args.obs_mode == 'survey':
-        pars['nreader'] = 2
+        pars['nreader'] = 5
     elif args.obs_mode == 'amber':
-        pars['nreader'] = 1
+        pars['nreader'] = 4
     else:
         pars['nreader'] = 1
 
