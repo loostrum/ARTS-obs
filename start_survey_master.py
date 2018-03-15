d47ab3bec0a40d75df01946ca6e0580e24d2a233
Leon Oostrum <leonoostrum@gmail.com>
2018-03-15 15:38:18 +0000

    less precision in RA/DEC, plus bugfix

--- start_survey_master.py
+++ start_survey_master.py
@@ -322,10 +322,10 @@ def start_survey(args):
 
         # save the coordinates of the beams
         this_cb_coord = pointing_to_CB_pos(beam, coord)
-        gl, gb = coord.galactic.to_string(precision=8).split(' ')
-        alt, az = coord.transform_to(AltAz(obstime=starttime, location=wsrt_loc)).to_string().split(' ')
-        ra = this_cb_coord.ra.to_string(unit=u.hourangle, sep=':', pad=True)
-        dec = this_cb_coord.dec.to_string(unit=u.degree, sep=':', pad=True)
+        gl, gb = this_cb_coord.galactic.to_string(precision=8).split(' ')
+        alt, az = this_cb_coord.transform_to(AltAz(obstime=starttime, location=wsrt_loc)).to_string().split(' ')
+        ra = this_cb_coord.ra.to_string(unit=u.hourangle, sep=':', pad=True, precision=1)
+        dec = this_cb_coord.dec.to_string(unit=u.degree, sep=':', pad=True, precision=1)
         coordinates.append(["{:02d}".format(beam), ra, dec, gl, gb])
 
         # fill in the psrdada header keys 
