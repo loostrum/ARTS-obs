e4f8ffa53f048da0a3aae1693f4acf2d5cdccc4a
Leon Oostrum <leonoostrum@gmail.com>
2018-03-23 12:57:22 +0000

    bugfixed and added plotter

--- utilities/trigger_sky_location.py
+++ utilities/trigger_sky_location.py
@@ -8,7 +8,7 @@ import sys
 import argparse
 
 import numpy as np
-import matplotlib.pyplot as plot
+import matplotlib.pyplot as plt
 from astropy import units as u
 from astropy.time import Time, TimeDelta
 from astropy.coordinates import SkyCoord, EarthLocation, AltAz
@@ -18,9 +18,9 @@ from sigpyproc.Readers import FilReader
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Plot azimuth and elevation dependence of triggers")
     # path to trigger file
-    parser.add_argument("--trigger", "-t", type=str, help="Path to .trigger or .singlepulse file")
+    parser.add_argument("triggers", type=str, help="Path to .trigger or .singlepulse file")
     # path to filterbank file (for header)
-    parser.add_argument("--filterbank", "-f", type=str, help="Path to filterbank filterbank file")
+    parser.add_argument("filterbank", type=str, help="Path to filterbank filterbank file")
 
     args = parser.parse_args()
 
@@ -28,44 +28,51 @@ if __name__ == '__main__':
     if not os.path.isfile(args.filterbank):
         print "Cannot find filterbank file {}".format(args.filterbank)
         sys.exit(1)
-    if not os.path.isfile(args.trigger):
+    if not os.path.isfile(args.triggers):
         print "Cannot find trigger file {}".format(args.trigger)
         sys.exit(1)
 
     # get header info
     hdr = FilReader(args.filterbank).header
     radec = SkyCoord(hdr.ra, hdr.dec, unit=(u.hourangle, u.deg))
-    starttime = Time(hdr.utcstart, format='iso', scale='utc')
+    starttime = Time(hdr.tstart, format='mjd', scale='utc')
 
     # define coordinates
     wsrt = EarthLocation(lat=52.915184*u.deg, lon=6.60387*u.deg, height=0*u.m)
     altazstart = radec.transform_to(AltAz(obstime=starttime, location=wsrt))
 
     # check alt az start
-    assert hdr.az_start == altazstart.az.deg and hdr.za_start == (90 - altazstart.alt.deg)
-
+    assert np.abs(hdr.az_start - altazstart.az.deg) < 1E-4 and \
+           np.abs(hdr.za_start - (90 - altazstart.alt.deg)) < 1E-4
 
     # load triggers
-    triggers = np.loadtxt(args.trigger, unpack=True)
-    if args.trigger.endswith('.trigger')
+    triggers = np.loadtxt(args.triggers, unpack=True)
+    if args.triggers.endswith('.trigger'):
         try:
-            dm, sigma, t0, downs = triggers[6, 8, 5, 3]
+            dm, sigma, t0, downs = triggers[6], triggers[8], triggers[5], triggers[3]
         except:
-            dm, sigma, t0, downs = triggers[-2, -1, -3 ,3]
-    elif args.trigger.endswith('.singlepulse'):
-        dm, sigma, t0. downs = triggers[0, 1, 2, 4]
+            dm, sigma, t0, downs = triggers[-2], triggers[-1], triggers[-3], triggers[3]
+    elif args.triggers.endswith('.singlepulse'):
+        try:
+            dm, sigma, t0, downs = triggers[0], triggers[1], triggers[2], triggers[4]
+        except:
+            dm, sigma, t0, downs = triggers[0], triggers[1], triggers[2], triggers[3]
     else:
         print "Can only load AMBER or PRESTO trigger files"
         sys.exit(1)
+    print "Found {} triggers".format(len(t0))
 
     # get alt and az of each trigger
-    altarray = np.zeros(len(t0))
-    azarray = np.zers(len(t0))
-    for i, t in enumerate(t0):
-        utc_trigger = starttime + TimeDelta(t0, format='sec')
-        altaz = radec.transform_to(AltAz(obstime=utc_trigger), location=wsrt)
-        altarray[i] = altaz.alt.deg
-        azarray[i] = altaz.az.deg
+    utc_trigger = starttime + TimeDelta(t0, format='sec')
+    altaz = radec.transform_to(AltAz(obstime=utc_trigger, location=wsrt))
+    altarray = altaz.alt.deg
+    azarray = altaz.az.deg
 
-    print altarray
-    print azarray
+    # create plot
+    bins = 20
+    fig ,ax = plt.subplots()
+    cax = ax.hist2d(azarray, altarray, bins=bins)[-1]
+    cbar = fig.colorbar(cax)
+    ax.set_xlabel('Azimuth (deg)')
+    ax.set_ylabel('Altitude (deg)')
+    plt.savefig('altaz.pdf')
