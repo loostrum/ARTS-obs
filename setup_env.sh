18e514fb0d380e77a63c36b4cd2633407a5a2d5f
Leon Oostrum <leonoostrum@gmail.com>
2018-03-28 14:24:49 +0000

    moved some script to utils. added alias for auto gain setting

--- setup_env.sh
+++ setup_env.sh
@@ -9,20 +9,23 @@ MASTER="arts041"
 SOURCE_DIR=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
 # directory of control scripts
 CONTROL=$SOURCE_DIR/control
+# directory of utility scripts
+UTIL=$SOURCE_DIR/utilities
 
 # scripts only for master node
 if [ "$(hostname)" == "$MASTER" ]; then
     alias start_streams=$CONTROL/start_streams.sh
     alias set_gain=$CONTROL/set_gain.sh
     alias read_gain=$CONTROL/read_gain.sh
+    alias auto_gain=$SOURCE_DIR/auto_gain/set_auto_gain.py
     alias point_array=$CONTROL/point_array.sh
     alias start_obs=$SOURCE_DIR/start_survey_master.py
 fi
 
 # scripts for all nodes
 alias CB_to_offset=$CONTROL/CB_to_offset.py
-alias get_ha=$CONTROL/get_ha.py
-alias psr_ra_dec=$CONTROL/psr_ra_dec.sh
+alias get_ha=$UTIL/get_ha.py
+alias psr_ra_dec=$UTIL/psr_ra_dec.sh
 alias packet_rate=$CONTROL/packet_rate.py
 # ARTS041 40g link is down, so allow on all nodes for now
 alias check_40g_links=$CONTROL/check_40g_links.py
