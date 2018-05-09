e5336528da8de3316f8ef63b287c7f52e9ea8145
Leon Oostrum <leonoostrum@gmail.com>
2018-05-09 13:17:46 +0000

    Add script for fly's eye TAB weights

---
+++ control/tab_weights_flys_eye.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#
+# set TAB beamformer weights for fly's eye mode
+# TAB 00 = RT2
+# TAB 01 = RT3
+# TAB 02 = RT4
+# TAB 03 = RT5
+# TAB 04 = RT6
+# TAB 05 = RT7
+# TAB 06 = RT8
+# TAB 07 = RT9
+# TAB 08 = RTA
+# TAB 09 = RTB
+# TAB 10 = RTC
+# TAB 11 = RTD
+
+
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 bands, weight"
+    echo "E.g. for setting up fly's eye on bands 2 to 15 with weight 31 for both Re and Im:"
+    echo "$0 2:15 31,31"
+    exit
+fi
+
+unbs="$1"
+weights="$2"
+
+
+for i in {00..11}; do 
+    ssh -t arts@ccu-corr.apertif python /home/arts/SVN/UniBoard/trunk/Software/python/peripherals/pi_arts_tab_beamformer_weights.py --cmd 3 --inputs $i --tabs $i --unb $unbs --fn 0:3 --bn 0:3 --weight $weights
+done
