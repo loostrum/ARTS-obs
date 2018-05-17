8550afc4510b982c251c398df2d9c4b231ed73b4
Leon Oostrum <leonoostrum@gmail.com>
2018-05-17 09:47:49 +0000

    fixed fly's eye weights, needed append option

--- control/set_tab_weights_flys_eye.sh
+++ control/set_tab_weights_flys_eye.sh
@@ -25,7 +25,9 @@ fi
 unbs="$1"
 weights="$2"
 
+# first set all weights to zero
+ssh -t arts@ccu-corr.apertif python /home/arts/SVN/UniBoard/trunk/Software/python/peripherals/pi_arts_tab_beamformer_weights.py --cmd 3 --input 0:11 --tabs 0:11 --unb $unbs --fn 0:3 --bn 0:3 --weight 0,0
 
 for i in {00..11}; do 
-    ssh -t arts@ccu-corr.apertif python /home/arts/SVN/UniBoard/trunk/Software/python/peripherals/pi_arts_tab_beamformer_weights.py --cmd 3 --inputs $i --tabs $i --unb $unbs --fn 0:3 --bn 0:3 --weight $weights
+    ssh -t arts@ccu-corr.apertif python /home/arts/SVN/UniBoard/trunk/Software/python/peripherals/pi_arts_tab_beamformer_weights.py --cmd 3 --input $i --tabs $i --unb $unbs --fn 0:3 --bn 0:3 -a --weight $weights
 done
