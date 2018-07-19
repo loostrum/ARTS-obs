081859d282d560dcd93af2c4d532e5164b3f2c57
Leon Oostrum <leonoostrum@gmail.com>
2018-07-19 10:28:15 +0200

    make template easier to read (long lines though)

--- utilities/heimdall_processing.py
+++ utilities/heimdall_processing.py
@@ -3,8 +3,10 @@
 # Process ARTS filterbank files
 # -- Run Heimdall
 # -- Group resulting candidate files and extract dedispersed data (triggers.py)
+# -- Run ML classifier (classify.py)
 # -- Plot candidates (plotter.py)
 # -- merge into one pdf per compound beam (bash)
+# -- Put archive in the arts home dir and notify people through slack
 
 
 import os
@@ -138,7 +140,6 @@ class Processing(object):
             close_fds = True
         else:
             close_fds = False
-        #command = ['ssh', hostname, "'{}'".format(command)]
         sys.stdout.write("Executing \"{}\" on {}\n".format(command, hostname))
         proc = subprocess.Popen(['ssh', hostname, command], close_fds=close_fds)
         return proc
@@ -166,20 +167,17 @@ class Processing(object):
         localconfig['fhi'] = self.config['freq'] + .5*self.config['bw'] - .5*chan_width
 
         # command to run
-        heimdall_command = ("(rm -f {heimdall_dir}/*cand; heimdall -beam {CB} -v -f {filfile} -dm 0 {dmmax} -gpu_id 0 "
-                            " -output_dir {heimdall_dir}; cd {heimdall_dir}; "
-                            " cat *cand > CB{CB:02d}.cand) 2>&1 > {result_dir}/CB{CB:02d}_heimdall.log").format(CB=CB, **localconfig)
-        trigger_command = ("(cd {heimdall_dir}; mkdir plots; mkdir data; "
-                           " python $HOME/software/arts-analysis/triggers.py --dm_min 10 --dm_max {dmmax} "
-                           " --sig_thresh {snrmin} --ndm 32 --save_data concat --nfreq_plot 32 "
-                           " --ntime_plot 250 --cmap viridis {filfile} CB{CB:02d}.cand; "
-                           " source $HOME/python/bin/activate; "
-                           " python $HOME/software/single_pulse_ml/single_pulse_ml/classify.py "
-                           " --pthresh 0.1 --save_ranked data/data_full.hdf5 ~/keras_models/keras_model_20000_artsfreq_time.hdf5; "
-                           " python $HOME/software/arts-analysis/plotter.py data/data_fullfreq_time_candidates.hdf5 {CB:02d} {flo} {fhi}; "
-                           " gs -dBATCH -dNOPAUSE -q -sDEVICE=pdfwrite "
-                           " -dPDFSETTINGS=/prepress -sOutputFile={result_dir}/CB{CB:02d}.pdf plots/*pdf) "
-                           " 2>&1 > {result_dir}/CB{CB:02d}_trigger.log").format(CB=CB, **localconfig)
+        heimdall_command = """(rm -f {heimdall_dir}/*cand; heimdall -beam {CB} -v -f {filfile} -dm 0 {dmmax} -gpu_id 0 -output_dir {heimdall_dir}
+            cd {heimdall_dir}
+            cat *cand > CB{CB:02d}.cand) 2>&1 > {result_dir}/CB{CB:02d}_heimdall.log")""".format(CB=CB, **localconfig)
+
+        trigger_command = """"(cd {heimdall_dir}; mkdir plots; mkdir data;
+            python $HOME/software/arts-analysis/triggers.py --dm_min 10 --dm_max {dmmax} --sig_thresh {snrmin} --ndm 32 --save_data concat --nfreq_plot 32 --ntime_plot 250 --cmap viridis {filfile} CB{CB:02d}.cand
+            source $HOME/python/bin/activate;
+            python $HOME/software/single_pulse_ml/single_pulse_ml/classify.py --pthresh 0.1 --save_ranked data/data_full.hdf5 $HOME/keras_models/keras_model_20000_artsfreq_time.hdf5
+            deactivate
+            python $HOME/software/arts-analysis/plotter.py data/data_fullfreq_time_candidates.hdf5 {CB:02d} {flo} {fhi}
+            gs -dBATCH -dNOPAUSE -q -sDEVICE=pdfwrite -dPDFSETTINGS=/prepress -sOutputFile={result_dir}/CB{CB:02d}.pdf plots/*pdf) 2>&1 > {result_dir}/CB{CB:02d}_trigger.log""".format(CB=CB, **localconfig)
         full_command = '; '.join([heimdall_command, trigger_command])
         if self.config['app'] == 'heimdall':
             command = heimdall_command
