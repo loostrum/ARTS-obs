ebd7f8548ebba81d30094d808d705940a4648635
Leon Oostrum <leonoostrum@gmail.com>
2018-08-28 16:06:55 +0000

    update triggers processing to new classifier and arts-analysis

--- process_triggers.sh
+++ process_triggers.sh
@@ -7,20 +7,22 @@
 SOURCE_DIR=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
 
 triggerscript=$HOME/software/arts-analysis/triggers.py
-preproc=$HOME/software/arts-analysis/preprocess.py
-classifier=$HOME/software/single_pulse_ml/single_pulse_ml/run_single_pulse_DL.py
+classifier=$HOME/software/single_pulse_ml/single_pulse_ml/classify.py
 trigger_to_master=$SOURCE_DIR/trigger_to_master.py
 plotter=$HOME/software/arts-analysis/plotter.py
 # python venv location
 venv_dir=$HOME/python
 
 cmap=viridis
-ntime_plot=250
+ntime_plot=64
 nfreq_plot=32
-ndm=1
-fmt=hdf5
+ndm=64
+fmt=concat
 dmmin=10
 dmmax=5000
+modeldir=$HOME/keras_models
+pthresh=0.0
+ML_GPUs=0
 
 outputdir=$1
 filfile=$2
@@ -28,34 +30,34 @@ prefix=$3
 master_dir=$4
 snrmin=$5
 CB=$6
-flo=$7
-fhi=$8
+
+# Set GPUs visible to the classifier
+export CUDA_VISIBLE_DEVICES=$ML_GPUs
 
 # create master trigger files
-cat ${prefix}_step*trigger > ${prefix}.trigger
+#cat ${prefix}_step*trigger > ${prefix}.trigger
 # get number of raw candidates
 ncand_raw=$(grep -v \# ${prefix}.trigger | wc -l)
 
 # make sure we start clean
-rm -f $outputdir/*hdf5
-rm -f $outputdir/plots/*pdf
 mkdir -p $outputdir/plots
+mkdir -p $outputdir/data
+rm -f $outputdir/data/*
+rm -f $outputdir/plots/*pdf
 cd $outputdir
 # process the triggers without making plots
-python $triggerscript --dm_min $dmmin --dm_max $dmmax --sig_thresh $snrmin --ndm $ndm --save_data $fmt --nfreq_plot $nfreq_plot --ntime_plot $ntime_plot --cmap $cmap $filfile ${prefix}.trigger
+python $triggerscript --mk_plot --dm_min $dmmin --dm_max $dmmax --sig_thresh $snrmin --ndm $ndm --save_data $fmt --nfreq_plot $nfreq_plot --ntime_plot $ntime_plot --cmap $cmap --outdir=$outputdir $filfile ${prefix}.trigger
 # get number of triggers after grouping
 if [ ! -f grouped_pulses.singlepulse ]; then
     ncand_grouped=0
 else
     ncand_grouped=$(wc -l grouped_pulses.singlepulse | awk '{print $1}')
-    # concatenate hdf5 files
-    python $preproc --fnout combined.hdf5 --nfreq_f $nfreq_plot --ntime_f $ntime_plot $(pwd)
     # run the classifier
     source $venv_dir/bin/activate
-    python $classifier combined.hdf5
+    #python $classifier combined.hdf5
+    python $classifier --fn_model_dm $modeldir/heimdall_dm_time.hdf5 --fn_model_time $modeldir/heimdall_b0329_mix_147411d_time.hdf5 --pthresh $pthresh --save_ranked --plot_ranked --fnout=ranked_CB$CB $outputdir/data_full.hdf5 $modeldir/heimdall_b0329_mix_14741freq_time.hdf5
     deactivate
-    # make plots
-    python $plotter combinefreq_time_candidates.hdf5 $CB $flo $fhi
+    exit
     # merge and copy to master dir
     ncands=$(ls $outputdir/plots | wc -l)
     merged=candidates.pdf
