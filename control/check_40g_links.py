e3cc30b5bc399ba615c53cf0cfade604d19a3daf
ARTS <arts@arts041.apertif>
2019-01-25 12:52:35 +0000

    Add arts041 to 40gbit check

--- control/check_40g_links.py
+++ control/check_40g_links.py
@@ -7,19 +7,19 @@ import os
 import sys
 
 if __name__ == '__main__':
-    missing_beams = [4, 9, 14, 9, 24, 29, 34, 39]
-    beams = range(40)
+    beams = range(41)
 
     online_links = []
     for beam in beams:
-        if beam in missing_beams:
-            continue
         node = beam + 1
         hostname = "arts0{:02d}.40g.apertif".format(node)
         cmd = "ping -W 1 -c 1 {} > /dev/null".format(hostname)
         if os.system(cmd) == 0:
             online_links.append("{:02d}".format(beam))
         else:
-            sys.stderr.write("WARNING: skipping offline link: CB{:02d} @ {}\n".format(beam, hostname))
+            if beam == 40:
+                sys.stderr.write("WARNING: skipping offline link: Master @ {}\n".format(hostname))
+            else:
+                sys.stderr.write("WARNING: skipping offline link: CB{:02d} @ {}\n".format(beam, hostname))
 
     print ','.join(online_links)
