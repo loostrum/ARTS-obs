144163ae9cd4af8858a1b6e6b33c61b30a41a472
ARTS <arts@arts041.apertif>
2019-03-17 15:12:23 +0000

    Fix options in start_streams and use apertif account

--- control/start_streams.sh
+++ control/start_streams.sh
@@ -1,7 +1,7 @@
 # Author: Daniel van der Schuur
 # Adapted for any science case + bands + telescopes by Leon Oostrum
 
-if [ "$#" -ne 2 ] && [ "$#" -ne 3 ] && [ "$#" -ne 4 ]; then
+if [ "$#" -ne 2 ] && [ "$#" -ne 3 ] && [ "$#" -ne 4 ] && [ "$#" -ne 5 ]; then
     echo "Usage: $0 [iab] bands, telescopes, pol (optional), opts (optional)"
     echo "E.g. for starting 14 bands on RT4 and RT7:"
     echo "$0 2:15 4,7"
@@ -16,7 +16,7 @@ if [ "$1" == "IAB" ]; then
     app=arts_sc4-iab
     shift
 elif [ "$1" == "TAB" ]; then
-    echo "Starting IAB firmware"
+    echo "Starting TAB firmware"
     app=arts_sc4
     shift
 else
@@ -39,12 +39,6 @@ elif [ "$tels" == "a10" ]; then
     tels="2,3,4,5,6,7,8,9,a,b"
 fi
 
-if [ x"$opts" == x ]; then
-    opts=""
-else
-    opts="--opt $opts"
-fi
-
 if [ x"$pol" == x"0" ] || [ x"$pol" == x"1" ]; then
     # single pol
     pol="--pol $pol"
@@ -53,7 +47,8 @@ else
     pol=""
 fi
 
-ssh -t arts@ccu-corr.apertif python /home/arts/SVN/RadioHDL/trunk/applications/apertif/commissioning/main.py --app $app --tel $tels --unb $unbs $opts $pol
+#ssh -t arts@ccu-corr.apertif python /home/arts/SVN/RadioHDL/trunk/applications/apertif/commissioning/main.py --rerun --app $app --tel $tels --unb $unbs $opts $pol
+ssh -t apertif@ccu-corr.apertif python /home/apertif/UniBoard_FP7/RadioHDL/trunk/applications/apertif/commissioning/main.py --app $app --tel $tels --unb $unbs $opts $pol
 
 # only set gain for science mode (i.e. IAB)
 #if [ "$app" == "arts_sc4" ]; then
