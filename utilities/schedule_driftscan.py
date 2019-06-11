c891d08c136ef8ff9e961f4bfea879c95da1ffbd
ARTS <arts@arts041.apertif>
2019-06-11 08:59:56 +0000

    Add HA column for drift scans

--- utilities/schedule_driftscan.py
+++ utilities/schedule_driftscan.py
@@ -30,30 +30,35 @@ if __name__ == '__main__':
     # rows of PAF
     cb_sets = [(0,), range(1, 8), range(8, 15), range(15, 21), range(21, 27), range(27, 33), range(33, 40)]
 
-#    # 3C286
-#    src = '3C286'
-#    RA = c.Angle('13:31:08.2883035 hours').to(u.degree)
-#    Dec = c.Angle('+30:30:32.962130 degrees').to(u.degree)
+    # 3C286
+    src = '3C286'
+    RA = c.Angle('13:31:08.2883035 hours').to(u.degree)
+    Dec = c.Angle('+30:30:32.962130 degrees').to(u.degree)
 
 #    # 3C147
 #    src = '3C147'
 #    RA = c.Angle('05:42:36.1378984 hours').to(u.degree)
 #    Dec = c.Angle('+49:51:07.233725 degrees').to(u.degree)
 
+#    # 3C48
+#    src = '3C48'
+#    RA = c.Angle('01:37:41.2995845985 hours').to(u.degree)
+#    Dec = c.Angle('+33:09:35.079126038 degrees').to(u.degree)
+
     # Crab
-    src = 'B0531+21'
-    RA = c.Angle('05:34:31.973 hours').to(u.degree)
-    Dec = c.Angle('+22:00:52.06 degrees').to(u.degree)
+    #src = 'B0531+21'
+    #RA = c.Angle('05:34:31.973 hours').to(u.degree)
+    #Dec = c.Angle('+22:00:52.06 degrees').to(u.degree)
 
     # start times
     #stimes = ["2019-03-19 01:28:51", "2019-03-19 23:18:28", "2019-03-21 00:15:28", "2019-03-22 01:09:18" , "2019-03-23 02:00:29", "2019-03-23 23:44:49", "2019-03-25 00:41:07"]
-    stimes = ["2019-04-03 16:18:24"]
+    stimes = ["2019-06-05 15:47:00"]
 
     for t in stimes:
         fname = t.replace(' ', '_') + '.csv'
         with open(fname, 'w') as f:
             # add header
-            hdr = 'source,ra,dec,date1,time1,date2,time2,freq,weight,sbeam,ebeam,pulsar\n'
+            hdr = 'source,ra,ha,dec,date1,time1,date2,time2,centfreq,weight,sbeam,ebeam,pulsar,beam\n'
             f.write(hdr)
 
             print "Calculating drift scans of {} starting at {}".format(src, t)
@@ -87,8 +92,8 @@ if __name__ == '__main__':
 
         
                 # write pointing
-                pointing = "{},{},{},{},{},{},{},1400,square_39p1,0,39,False\n".format(src_drift,
-                            pointing_ha, pointing_dec, date1, time1, date2, time2)
+                pointing = "{},,{},{},{},{},{},{},1370,square_39p1,0,39,False,{}\n".format(src_drift,
+                            pointing_ha, pointing_dec, date1, time1, date2, time2, ref_beam)
                 f.write(pointing)
                 
 
